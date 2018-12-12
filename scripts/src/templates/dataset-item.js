4cb857770c683aceec2fdfbb784c47235b61f99d
keeganmcbride <keegan.mcbride@outlook.com>
2018-12-12 12:06:18 +0200

    add more info to dataset page

--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -1,6 +1,9 @@
 export default (data) => (
 `<dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
+  <p><b>Teabevaldaja:</b> ${data.organization || ''}
+  <br>
+  <b>Teema:</b> <i>${data.category || ''}</i></p>
   ${data.notes || ''}
 </dataset>`
 )
