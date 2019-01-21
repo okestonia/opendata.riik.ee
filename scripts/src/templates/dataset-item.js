6dca1aa4e62238a27bcdd04bd26b17b27072cf8e
keeganmcbride <keegan.mcbride@outlook.com>
2019-01-21 11:17:28 +0200

    dataset and organizations fix

--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -1,6 +1,6 @@
 export default (data) => (
 `<dataset>
-  <h3><a href="${data.url}">${data.title}</a></h3>
+  <h3 class="pt-3"><a href="${data.url}">${data.title}</a></h3>
   <p><b>Teabevaldaja:</b> ${data.organization || ''}
   <br>
   <b>Teema:</b> <i>${data.category || ''}</i></p>
