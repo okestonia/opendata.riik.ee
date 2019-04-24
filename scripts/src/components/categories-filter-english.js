0004a5062f0031be5452800053dc644183b8d4e6
keeganmcbride <keegan.mcbride@outlook.com>
2019-04-24 10:41:32 +0300

    work on english gilter

--- scripts/src/components/categories-filter-english.js
+++ scripts/src/components/categories-filter-english.js
@@ -18,22 +18,22 @@ export default class {
       .filter('category_en')
       .flatMap(function (value, index, collection) {
         // Explode objects where category is an array into one object per category
-        if (typeof value.category_en === 'string') return value
+        if (typeof value.category === 'string') return value
         const duplicates = []
-        value.category_en.forEach(function (category_en) {
-          duplicates.push(defaults({category_en: category_en}, value))
+        value.category.forEach(function (category) {
+          duplicates.push(defaults({category: category}, value))
         })
         return duplicates
       })
       .groupBy('category_en')
-      .map(function (datasetsInCat, category_en) {
+      .map(function (datasetsInCat, category) {
         const filters = createDatasetFilters(pick(params, ['organization']))
         const filteredDatasets = filter(datasetsInCat, filters)
-        const categorySlug = slugify(category_en)
-        const selected = params.category_en && params.category_en === categorySlug
-        const itemParams = selected ? omit(params, 'category_en') : defaults({category_en: categorySlug}, params)
+        const categorySlug = slugify(category)
+        const selected = params.category && params.category === categorySlug
+        const itemParams = selected ? omit(params, 'category_en') : defaults({category: categorySlug}, params)
         return {
-          title: category_en,
+          title: category,
           url: '?' + $.param(itemParams),
           count: filteredDatasets.length,
           unfilteredCount: datasetsInCat.length,
