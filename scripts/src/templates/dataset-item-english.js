fcaa825f5d56ba03b27ef60fa0776f85b7db2638
keeganmcbride <keegan.mcbride@outlook.com>
2019-07-22 10:23:07 +0300

    WCAG Fixes + JS fix

--- scripts/src/templates/dataset-item-english.js
+++ scripts/src/templates/dataset-item-english.js
@@ -3,7 +3,7 @@ export default (data) => (
   <h3 class="pt-3"><a href="${data.url_en}">${data.title_en}</a></h3>
   <p><b>Organization:</b> ${data.organization || ''}
   <br>
-  <b>Theme:</b> <i>${data.category_en || ''}</i></p>
+  <b>Theme:</b> <em>${data.category_en || ''}</em></p>
   ${data.notes_en || ''}
 </dataset>`
 )
--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -3,7 +3,7 @@ export default (data) => (
   <h3 class="pt-3"><a href="${data.url}">${data.title}</a></h3>
   <p><b>Teabevaldaja:</b> ${data.organization || ''}
   <br>
-  <b>Teema:</b> <i>${data.category || ''}</i></p>
+  <b>Teema:</b> <em>${data.category || ''}</em></p>
   ${data.notes || ''}
 </dataset>`
 )
